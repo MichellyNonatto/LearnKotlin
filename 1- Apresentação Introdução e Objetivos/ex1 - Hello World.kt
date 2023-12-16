0916b7bd1e4cf1a5d1176c7a7a9940d37753cf74
Michelly Nonato <smartnovo01@gmail.com>
2023-12-15 21:29:44 -0300

    Hello world!

---
+++ 1- Apresentação Introdução e Objetivos/ex1 - Hello World.kt
@@ -0,0 +1,7 @@
+package org.kotlinlang.play // 1. Definição do pacote (algo muito comum para a organização de
+// código)
+
+fun main() { // 2. Ponto de entrada de um código Kotlin, definido pela função [main]
+    println("Hello world!") // 3. Imprime na saída padrão um texto, pulando linha.
+    println("Olá mundo!")
+}
